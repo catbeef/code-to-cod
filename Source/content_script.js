87633cccdd4ca63cb21ebde516ac85d8f5e4c4e1
Misha Koryak <mkoryak@gmail.com>
2015-12-02 10:04:42 -0500

    change the source cod

--- Source/content_script.js
+++ Source/content_script.js
@@ -29,14 +29,7 @@ function walk(node)
 
 function handleText(textNode) 
 {
-	var v = textNode.nodeValue;
-
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+	textNode.nodeValue = textNode.nodeValue.replace(/\b(cod)e\b/gi, '$1');
 }
 
 
